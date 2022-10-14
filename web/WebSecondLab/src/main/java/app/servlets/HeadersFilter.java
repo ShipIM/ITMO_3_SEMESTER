7ab389ba520ce674c90e7d5e54def80c34dc7a2f
ShipIM <ilya.shipunov@gmail.com>
2022-10-14 22:09:04 +0300

    final

--- web/WebSecondLab/src/main/java/app/servlets/HeadersFilter.java
+++ web/WebSecondLab/src/main/java/app/servlets/HeadersFilter.java
@@ -6,33 +6,26 @@ import java.io.IOException;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
+@SuppressWarnings("unchecked")
 public class HeadersFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
 
         ServletContext context = request.getServletContext();
-        ConcurrentHashMap<String, Long> headers = new ConcurrentHashMap<>();
+        Object object = context.getAttribute("headers");
+        ConcurrentHashMap<String, Long> headers;
 
-        Object tmp = context.getAttribute("headers");
-        if (tmp != null) {
-            if (tmp instanceof ConcurrentHashMap<?, ?>) {
-                ConcurrentHashMap<?, ?> map = (ConcurrentHashMap<?, ?>) tmp;
-
-                map.forEach((key, value) -> {
-                    if (key instanceof String && value instanceof Long) {
-                        headers.put((String) key, (Long) value);
-                    }
-                });
-            }
+        if (object != null) {
+            headers = (ConcurrentHashMap<String, Long>) object;
+        } else {
+            headers = new ConcurrentHashMap<>();
+            context.setAttribute("headers", headers);
         }
 
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         Enumeration<String> names = httpRequest.getHeaderNames();
-
         Collections.list(names).forEach((name) -> headers.merge(name, 1L, Long::sum));
-
-        context.setAttribute("headers", headers);
 
         StringBuilder stringBuilder = new StringBuilder();
         headers.forEach((key, value) -> stringBuilder.append(key).append(" = ").append(value).append("\n"));
